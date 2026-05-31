c83cd54ad844ffd4ea72fcb6c6aba5da310085d8
arunnagra <arunnagra654@gmail.com>
2026-06-01 01:58:18 +0530

    new change

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import {
-  AuthProvider,
-} from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 
-const root =
-  ReactDOM.createRoot(
-    document.getElementById("root")
-  );
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 
 root.render(
   <AuthProvider>
